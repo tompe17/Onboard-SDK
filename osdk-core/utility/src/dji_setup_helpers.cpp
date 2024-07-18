d2ffd6dca1b97a1c7fa21068f388ea2f0a08bd39
Tommy Persson <tommy.persson@liu.se>
2024-07-18 17:48:17 +0200

    To get it to compile withput working functionality

--- osdk-core/utility/src/dji_setup_helpers.cpp
+++ osdk-core/utility/src/dji_setup_helpers.cpp
@@ -42,8 +42,8 @@ Setup::Setup(bool enableAdvancedSensing) {
 Setup::~Setup() {
 }
 
-void Setup::setupEnvironment() {
-}
+///void Setup::setupEnvironment() {
+///}
 
 bool Setup::initLinker() {
   /*! init linker */
