b730421a309fde2e7d2b3b926ea6364060d07396
Tommy Persson <tommy.persson@liu.se>
2025-04-03 10:57:06 +0200

    Fixes for jazzy

--- osdk-core/advanced-sensing/camera_stream/src/dji_camera_stream_decoder.cpp
+++ osdk-core/advanced-sensing/camera_stream/src/dji_camera_stream_decoder.cpp
@@ -259,7 +259,8 @@ void DJICameraStreamDecoder::decodeBuffer(uint8_t* buf, int bufLen)
           // bufSize = avpicture_get_size(AV_PIX_FMT_RGB24, w, h);
           bufSize = av_image_get_buffer_size(AV_PIX_FMT_RGB24, w, h, 1);
           rgbBuf = (uint8_t*) av_malloc(bufSize);
-          avpicture_fill((AVPicture*)pFrameRGB, rgbBuf, AV_PIX_FMT_RGB24, w, h);
+          avpicture_fill(pFrameRGB->data, rgbBuf, AV_PIX_FMT_RGB24, w, h);          
+          //avpicture_fill((AVPicture*)pFrameRGB, rgbBuf, AV_PIX_FMT_RGB24, w, h);
           // av_image_fill_arrays(pFrameRGB->data,pFrameRGB->linesize,(uint8_t*)rgbBuf,AV_PIX_FMT_RGB24, w, h, 1);
         }
 #endif
