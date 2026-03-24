10120571e54287761069bd7001e247944cecd6a0
Tommy Persson <tommy.persson@liu.se>
2026-03-24 17:22:08 +0100

    Adding test programm

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -283,7 +283,7 @@ ErrorCode::ErrorCodeType WaypointV2MissionSample::initMissionSetting(int timeout
   missionInitSettings.finishedAction = DJIWaypointV2MissionFinishedNoAction;
   //  missionInitSettings.finishedAction = DJIWaypointV2MissionFinishedGoToFirstWaypoint;
   missionInitSettings.maxFlightSpeed = 12.0;
-  missionInitSettings.autoFlightSpeed = 2.0;
+  missionInitSettings.autoFlightSpeed = 7.0;
   missionInitSettings.exitMissionOnRCSignalLost = 1;
   missionInitSettings.gotoFirstWaypointMode = DJIWaypointV2MissionGotoFirstWaypointModePointToPoint;
   // missionInitSettings.mission =  generatePolygonWaypoints(radius, polygonNum);
