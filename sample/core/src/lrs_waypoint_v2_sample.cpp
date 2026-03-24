b2d88c023a470dc9bf4ad99ef846b57153ac43ab
Tommy Persson <tommy.persson@liu.se>
2026-03-24 11:29:25 +0100

    Adding test programm

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -465,7 +465,7 @@ std::vector<WaypointV2> WaypointV2MissionSample::generateLineWaypoints(float32_t
   startPoint.relativeHeight = 15;
   setWaypointV2Defaults(startPoint);
   startPoint.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurve;
-  startPoint.dampingDistance = 40.0;
+  startPoint.dampingDistance = 0.0;
   waypointList.push_back(startPoint);
 
   // Iterative algorithm
@@ -473,7 +473,7 @@ std::vector<WaypointV2> WaypointV2MissionSample::generateLineWaypoints(float32_t
     setWaypointV2Defaults(waypointV2);
     float32_t X = step + i*step;
     float32_t Y = 0.0;
-    waypointV2.dampingDistance = 40.0;
+    waypointV2.dampingDistance = 0.0;
     waypointV2.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurve;    
     if (i == (n_points - 1)) {
       waypointV2.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurveAndStop;
@@ -513,7 +513,7 @@ void WaypointV2MissionSample::setWaypointV2Defaults(WaypointV2& waypointV2) {
   waypointV2.config.useLocalCruiseVel = 0;
   waypointV2.config.useLocalMaxVel = 0;
 
-  waypointV2.dampingDistance = 40.0;
+  waypointV2.dampingDistance = 0.0;
   waypointV2.heading = 0;
   waypointV2.turnMode = DJIWaypointV2TurnModeClockwise;
 
