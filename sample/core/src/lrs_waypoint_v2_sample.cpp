38b19caf66293c1e924ca9f6214f4801d32712ba
Tommy Persson <tommy.persson@liu.se>
2026-03-24 17:29:26 +0100

    Adding test programm

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -288,7 +288,7 @@ ErrorCode::ErrorCodeType WaypointV2MissionSample::initMissionSetting(int timeout
   missionInitSettings.gotoFirstWaypointMode = DJIWaypointV2MissionGotoFirstWaypointModePointToPoint;
   // missionInitSettings.mission =  generatePolygonWaypoints(radius, polygonNum);
   // missionInitSettings.mission =  generateLineWaypoints(10.0, 8);  
-  missionInitSettings.mission =  generateStairWaypoints(10.0, 8);  
+  missionInitSettings.mission =  generateStairWaypoints(100.0, 8);  
   missionInitSettings.missTotalLen = missionInitSettings.mission.size();
 
   ErrorCode::ErrorCodeType ret = vehiclePtr->waypointV2Mission->init(&missionInitSettings,timeout);
@@ -510,7 +510,7 @@ std::vector<WaypointV2> WaypointV2MissionSample::generateStairWaypoints(float32_
   /// waypointList.push_back(startPoint);
 
   // Iterative algorithm
-  float32_t X = 3*step;
+  float32_t X = 10.0;
   float32_t Y = 0.0;
   for (int i = 0; i < n_points; i++) {
     setWaypointV2Defaults(waypointV2);
