f4f087d05255ef7b3a99bd26a7ed6251640fcca1
Tommy Persson <tommy.persson@liu.se>
2026-03-25 10:24:15 +0100

    Adding test programm

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -551,9 +551,9 @@ std::vector<WaypointV2> WaypointV2MissionSample::generateAngleWaypoints(float32_
   startPoint.relativeHeight = 15;
   setWaypointV2Defaults(startPoint);
   startPoint.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurve;
-  startPoint.dampingDistance = 1.0;
+  startPoint.dampingDistance = 0.0;
   /// waypointList.push_back(startPoint);
-
+  
   // Iterative algorithm
   float32_t X = 20.0;
   float32_t Y = 0.0;
@@ -566,8 +566,8 @@ std::vector<WaypointV2> WaypointV2MissionSample::generateAngleWaypoints(float32_
     }
     Y += step;
     waypointV2.dampingDistance = 0.0;
-    /// waypointV2.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurve;
-    waypointV2.waypointType = DJIWaypointV2FlightPathModeCoordinateTurn;
+    waypointV2.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurve;
+    ///waypointV2.waypointType = DJIWaypointV2FlightPathModeCoordinateTurn;
     if (i == (n_points - 1)) {
       waypointV2.waypointType = DJIWaypointV2FlightPathModeGoToPointAlongACurveAndStop;
       waypointV2.dampingDistance = 0.0;      
