03efcddcc3ef6176240162f01ae72a970ec29d77
Piotr Rudol <piotr.rudol@liu.se>
2026-03-26 14:20:34 +0100

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -779,7 +779,7 @@ WaypointV2MissionSample::generateAngleWaypoints(float32_t step,
 //    startPoint.waypointType    = DJIWaypointV2FlightPathModeCoordinateTurn;
     startPoint.waypointType    = DJIWaypointV2FlightPathModeGoToPointInAStraightLineAndStop;
   startPoint.dampingDistance = 0.0;
-//  waypointList.push_back(startPoint);
+  waypointList.push_back(startPoint);
 
   printWpInfo(startPoint, "start wp");
 
