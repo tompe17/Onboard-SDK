dca199ddccb76022d26c718263689d24fc3e9a82
Piotr Rudol <piotr.rudol@liu.se>
2026-03-26 11:52:22 +0100

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -387,7 +387,7 @@ WaypointV2MissionSample::initMissionSetting(int timeout)
   // missionInitSettings.mission =  generatePolygonWaypoints(radius,
   // polygonNum); missionInitSettings.mission =  generateLineWaypoints(10.0, 8);
   // missionInitSettings.mission =  generateStairWaypoints(20.0, 6);
-  missionInitSettings.mission      = generateAngleWaypoints(10.0, 10.0, 4);
+  missionInitSettings.mission      = generateAngleWaypoints(10.0, 10.0, 2);
   missionInitSettings.missTotalLen = missionInitSettings.mission.size();
 
   printWaypointDistances(missionInitSettings.mission);
