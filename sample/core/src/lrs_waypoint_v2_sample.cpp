b90eb3d3b4f94e30ce2f69ef9c64a39f8ac268dd
Piotr Rudol <piotr.rudol@liu.se>
2026-03-27 13:24:43 +0100

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -821,7 +821,7 @@ WaypointV2MissionSample::generateAngleWaypoints(float32_t step,
   }
 
   setWaypointV2Defaults(startPoint);
-  waypointV2.headingMode    = DJIWaypointV2HeadingWaypointCustom;
+  waypointV2.headingMode    = DJIWaypointV2HeadingManual;
   waypointV2.heading        = 45.0;
   startPoint.latitude       = (subscribeGPosition.latitude);
   startPoint.longitude      = (subscribeGPosition.longitude);
@@ -845,7 +845,7 @@ WaypointV2MissionSample::generateAngleWaypoints(float32_t step,
   for (int i = 0; i < n_points; i++)
   {
     setWaypointV2Defaults(waypointV2);
-    waypointV2.headingMode = DJIWaypointV2HeadingWaypointCustom;
+    waypointV2.headingMode = DJIWaypointV2HeadingManual;
     waypointV2.heading     = 45.0;
     auto [dx, dy]          = rotateVector(step, a_rad);
 
