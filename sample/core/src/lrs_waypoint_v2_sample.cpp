73a1142f2ccb61e28f416602727ea6c13090d8d9
Piotr Rudol <piotr.rudol@liu.se>
2026-03-30 15:00:17 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -818,7 +818,7 @@ WaypointV2MissionSample::getDampingFactor(float32_t dampingFactor,
   // but making it a bit smaller seems to work more reliably
 
   // 0.0 is also somtimes problematic. Therefore 0.2 is min, 0.490 max
-  double halfWpDistance = wpDistanceM*0.5;
+  double halfWpDistance = wpDistanceM*0.44;
   halfWpDistance+=0.2*halfWpDistance;
   halfWpDistance = std::min(halfWpDistance,wpDistanceM*0.5);
   return (uint16_t)(halfWpDistance * f * 100.0);
