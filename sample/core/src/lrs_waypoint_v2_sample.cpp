458f4fc74b3a6ca312408db5cc11fb504f4a4d18
Piotr Rudol <piotr.rudol@liu.se>
2026-03-30 15:25:47 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -819,7 +819,7 @@ WaypointV2MissionSample::getDampingFactor(float32_t dampingFactor,
   // and having a minimum is also more reliable
   // both 0.4 and 0.1 are experimental
   double halfWpDistance = wpDistanceM*0.4*f;
-  double minimum = 0.001*wpDistanceM;
+  double minimum = 0.1*wpDistanceM;
   if (halfWpDistance < minimum)
       halfWpDistance = minimum;
 
