7ff6f76e01c3599a0daa0f32b4022c37bb5b3a8c
Piotr Rudol <piotr.rudol@liu.se>
2026-03-31 15:07:24 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -421,10 +421,10 @@ WaypointV2MissionSample::sanityCheckMission(
     if (wp.waypointType == DJIWaypointV2FlightPathModeCoordinateTurn)
     {
       double dist = calculateDistance3D(wp_prev, wp);
-      if (dist <= 1.0)
+      if (dist < 1.0)
       {
         printf("ERROR: The waypoint [%zu] is type Curve and distance to "
-               "previous wp is %f (<=1.0)\n",
+               "previous wp is %f (<1.0)\n",
                i,
                dist);
         return false;
