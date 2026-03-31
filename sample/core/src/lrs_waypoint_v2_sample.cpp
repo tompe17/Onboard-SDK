dc09225205ee4883bd057aca7c8acf2a6eb5205f
Piotr Rudol <piotr.rudol@liu.se>
2026-03-31 11:29:12 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -1047,7 +1047,7 @@ WaypointV2MissionSample::generateAngleWaypoints(float32_t step,
 
     waypointList.push_back(wp);
   }
-  waypointList[1].dampingDistance = 400*1.5;
+  waypointList[1].dampingDistance = 500;
   return waypointList;
 }
 
