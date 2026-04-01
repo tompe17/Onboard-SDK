13369c9ca7ffc45681708d73b7818c3e9197d47e
Piotr Rudol <piotr.rudol@liu.se>
2026-04-01 10:06:12 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -439,7 +439,7 @@ WaypointV2MissionSample::sanityCheckMission(
 
     const double EPS = 1e-9;
 //    if (x < 3.0 - EPS)
-    if (angle <= 3.0-EPS)
+    if (angle < 3.0-EPS)
     {
       printf("Angle at WPs[%zu->%zu->%zu]: %.2f deg (<=3.0)\n",
              i - 1,
