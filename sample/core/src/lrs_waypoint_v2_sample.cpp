dbc4f1674d9aaf6bff97eab31b86d398b11d3972
Piotr Rudol <piotr.rudol@liu.se>
2026-04-01 09:47:57 +0200

    pioru testing

--- sample/core/src/lrs_waypoint_v2_sample.cpp
+++ sample/core/src/lrs_waypoint_v2_sample.cpp
@@ -1153,6 +1153,7 @@ WaypointV2MissionSample::generateWaypoints(GenParams& params)
     auto a =wp_prev.dampingDistance;
     auto b = wp.dampingDistance;
     float percent = 0.1;
+  if (i>0)
     if (std::abs(a - b) > percent * std::abs(a))
 
 //    if (wp_prev.dampingDistance > wp.dampingDistance)
